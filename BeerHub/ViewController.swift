d77d76a86993f3aa84494175e55ed2bc291bd3a7
zenghao0708 <zenghao0708@gmail.com>
2018-06-24 10:48:41 +0800

    Initial Commit

---
+++ BeerHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerHub
+//
+//  Created by zenghao on 2018/6/24.
+//  Copyright © 2018 zenghao0708. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
